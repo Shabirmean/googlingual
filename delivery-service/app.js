c244587160966fea3ffa580a72dd0710155f48b7
Shabir Abdul Samadh <7249208+Shabirmean@users.noreply.github.com>
2021-01-22 00:50:59 -0500

    Fix socket emit error

--- delivery-service/app.js
+++ delivery-service/app.js
@@ -77,7 +77,7 @@ function listenForMessages() {
     console.log(`\tAttributes: ${message.attributes}`);
     messageCount += 1;
     message.ack();
-    io.emit('chatRoomMessage', message);
+    socketsMap[message.data].emit('chatRoomMessage', message);
   };
 
   subscription.on('message', messageHandler);
