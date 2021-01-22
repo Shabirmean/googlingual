837f1ebf47a5c5d64bec4aa7faa6120b3abdc801
Shabir Abdul Samadh <7249208+Shabirmean@users.noreply.github.com>
2021-01-21 23:42:25 -0500

    Add PubSub subscriber code

--- delivery-service/app.js
+++ delivery-service/app.js
@@ -14,9 +14,10 @@
 
 'use strict';
 
+const {PubSub} = require('@google-cloud/pubsub');
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
   cors: {
@@ -61,12 +62,36 @@ io.on('disconnect', socket => {
   }
 });
 
+const subscriptionName = 'projects/gcloud-dpe/subscriptions/delivery-service-subscription';
+const timeout = 60;
+const pubSubClient = new PubSub();
+
+function listenForMessages() {
+  const subscription = pubSubClient.subscription(subscriptionName);
+
+  let messageCount = 0;
+  const messageHandler = message => {
+    console.log(`Received message ${message.id}:`);
+    console.log(`\tData: ${message.data}`);
+    console.log(`\tAttributes: ${message.attributes}`);
+    messageCount += 1;
+    message.ack();
+  };
+
+  subscription.on('message', messageHandler);
+  setTimeout(() => {
+    subscription.removeListener('message', messageHandler);
+    console.log(`${messageCount} message(s) received.`);
+  }, timeout * 1000);
+}
+
 if (module === require.main) {
   const PORT = process.env.PORT || 8081;
   server.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
     console.log('Press Ctrl+C to quit.');
   });
+  listenForMessages();
 }
 
 module.exports = server;
