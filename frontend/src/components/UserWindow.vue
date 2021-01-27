3adc3f7328de0bcf1834231caf82cc56d8e0c1ee
Shabir Abdul Samadh <7249208+Shabirmean@users.noreply.github.com>
2021-01-27 12:03:31 -0500

    Fix error with callback with unbdefined

--- frontend/src/components/UserWindow.vue
+++ frontend/src/components/UserWindow.vue
@@ -123,7 +123,7 @@ export default {
       if (localesResp && localesResp.status === 200) {
         this.voices = this.hasElements(localesResp.data.results) ? localesResp.data.results : [ 'None' ];
       } else if (this.voicesGiveupCount < 10) {
-        setTimeout(this.loadAudioLocales, 7500);
+        setTimeout(this.loadAudioLocales.bind(null, lang), 7500);
       }
       this.fetchingVoices = false;
     },
